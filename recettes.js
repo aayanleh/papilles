20596c98c38f40670f78b538081e6a8b7c1df030
MAHDI Ayanleh <amahdi@ecritel.net>
2026-06-07 21:23:28 +0200

    volumes des sons 2

--- recettes.js
+++ recettes.js
@@ -1,4 +1,4 @@
-const version = '2.6';
+const version = '2.7';
 const RECETTES = {
     plats: [
         {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2.6';
+const APP_VERSION = '2.7';
 const CACHE_NAME = `papilles-v${APP_VERSION}`;
 const APP_SHELL = [
   './',
